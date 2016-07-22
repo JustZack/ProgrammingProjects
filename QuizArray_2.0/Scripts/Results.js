5798f488d91a79f50c8c6c37e72a69f66b78cb60
JustZack <justzackj@gmail.com>
2016-07-22 17:53:16 -0600

    Fixed F5 disable function in Results.js
    Remove the .on function from my code because it is no longer supported.

--- QuizArray_2.0/Scripts/Results.js
+++ QuizArray_2.0/Scripts/Results.js
@@ -2,14 +2,12 @@ $(document).ready(function()
 {
   $("#Submit-Button").click(function()
   {
-    console.log('ekke');
+    //Go back to the main page
     window.location.href = 'index.php';
   })
-  // slight update to account for browsers not supporting e.which
+
+//Disables the users ability to press F5 to refresh the page.
+//Both functions were found on stack overflow. I did not makes these.
 function disableF5(e) { if ((e.which || e.keyCode) == 116) e.preventDefault(); };
-// To disable f5
-    /* jQuery < 1.7 */
 $(document).bind("keydown", disableF5);
-/* OR jQuery >= 1.7 */
-$(document).on("keydown", disableF5);
 });
